16b7c1b97f458454efe554f13b4c2f07ee959d7a
n4o847 <22975590+n4o847@users.noreply.github.com>
2024-06-09 18:00:03 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,7 @@
+# flask-react-example
+
+バックエンドに Flask、フロントエンドに React を使用して、路線図を表示するなるべく小さい動作例です。
+
+![](docs/screenshot.png)
+
+ドキュメントは Cosense（旧 Scrapbox）で随時更新していきます。そちらの該当ページを見てください。
---
+++ src/flask_react_example/main.py
@@ -0,0 +1,43 @@
+import time
+from threading import Thread
+
+from flask import Flask, jsonify
+
+# サーバの設定です。
+app = Flask(__name__, static_url_path="", static_folder="../../ui/dist")
+
+
+# 路線の状態です。
+state = {
+    "trains": {
+        "t0": {"mileage": 0.0},
+    },
+}
+
+
+# 状態を取得する API です。
+@app.route("/api/state")
+def get_state():
+    return jsonify(state)
+
+
+# 状態を更新するループです。
+def update_state_loop():
+    while True:
+        state["trains"]["t0"]["mileage"] = (
+            state["trains"]["t0"]["mileage"] + 0.5
+        ) % 100.0
+        time.sleep(0.1)
+
+
+def main():
+    # 状態を更新するためのスレッドを立てます。
+    update_state_thread = Thread(target=update_state_loop, daemon=True)
+    update_state_thread.start()
+
+    # サーバを起動します。
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
